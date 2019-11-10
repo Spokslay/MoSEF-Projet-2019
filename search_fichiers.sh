e6cdc0e397adca9e189c2f8a1ce56725d8a67052
Elmehdi OUIZZA <elmehdi.ouizza@gmail.com>
2019-11-10 11:08:58 +0100

    modification du fichier search_fichiers.sh

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 Now=$(date +"%d %B %Y")
-echo "Bonjour Elmehdi nous sommes le $Now"
+echo "Bonjour $USERNAME,  nous sommes le $Now"
 read -p 'Quel répertoire vous intéresse aujourdhui ?' Rep
-echo "$Rep"
+echo "Vous avez saisi :  $Rep"
+
 
