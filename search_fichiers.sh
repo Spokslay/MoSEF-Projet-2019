62fa64e4a884ecd5c6a63d29391096882dc14ebc
Elmehdi OUIZZA <elmehdi.ouizza@gmail.com>
2019-11-14 21:54:37 +0100

    Correction erreur au niveau du Script

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -3,7 +3,6 @@ Now=$(date +"%d %B %Y")
 echo "Bonjour $USERNAME,  nous sommes le $Now"
 read -p 'Quel répertoire vous intéresse aujourdhui ?' Rep
 echo "Vous avez saisi :  $Rep"
-find ${Rep} -name $1
 echo "La liste des fichiers avec le pattern $1 dans votre chemin contenant $2 est :"
-find ${chemin} -name $1 -exec grep -il $2 {} \
+find ${Rep} -name $1 -exec grep -il $2 {} \
 
