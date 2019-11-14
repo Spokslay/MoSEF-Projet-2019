72153282cd9359acd1e36d08f11b585a026770a7
Elmehdi OUIZZA <elmehdi.ouizza@gmail.com>
2019-11-14 13:28:23 +0100

    modification du  script dans la nouvelle branche pour qu'il affiche tous les fichiers dont le nom respecte un pattern fourni en paramètre

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -3,5 +3,5 @@ Now=$(date +"%d %B %Y")
 echo "Bonjour $USERNAME,  nous sommes le $Now"
 read -p 'Quel répertoire vous intéresse aujourdhui ?' Rep
 echo "Vous avez saisi :  $Rep"
-
+find ${Rep} -name $1
 
